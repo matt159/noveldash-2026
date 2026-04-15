df44922df8c546f482fbb546ca427b8e99d29a90
matt159 <matthamilton@live.co.uk>
2026-04-15 21:07:09 +0000

    judge announce date config

--- config/dates.php
+++ config/dates.php
@@ -3,6 +3,7 @@
 return [
     'prize_opening_date' => env('PRIZE_OPENING_DATE'),
     'prize_closing_date' => env('PRIZE_CLOSING_DATE'),
+    'judges_announcement_date_text' => env('JUDGES_ANNOUNCEMENT_DATE_TEXT'),
     'longlist_announcement_date_text' => env('LONGLIST_ANNOUNCEMENT_DATE_TEXT'),
     'top_100_announcement_date_text' => env('TOP_100_ANNOUNCEMENT_DATE_TEXT'),
     'shortlist_announcement_date_text' => env('SHORTLIST_ANNOUNCEMENT_DATE_TEXT'),
